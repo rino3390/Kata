be9c8bb909ca48849a6b43d501e7531e50e6cd8d
Rino <sherikawaii3390@gmail.com>
2022-05-17 06:35:57 +0800

    feat: Complete CountingDuplicates

---
+++ CountingDuplicates/Kata.cs
@@ -0,0 +1,17 @@
+﻿namespace Kata.CountingDuplicates;
+
+public class Kata
+{
+	public static int DuplicateCount(string str) {
+		str = str.ToLower();
+		var store = new List<char>();
+		foreach (var t in str.Where(t => !store.Contains(t) && str.Count(c=> c==t)>1)) {
+			store.Add(t);
+		}
+		return store.Count;
+	}
+
+	public static int DuplicateCountOneLane(string str) {
+		return str.ToLower().GroupBy(c => c).Count(c => c.Count() > 1);
+	}
+}
---
+++ CountingDuplicates/Test.cs
@@ -0,0 +1,31 @@
+﻿namespace Kata.CountingDuplicates;
+
+[TestFixture]
+public class Test
+{
+	[SetUp]
+	public void SetUp() {
+	}
+
+	[Test]
+	[TestCase("",0)]
+	[TestCase("abcde",0)]
+	[TestCase("aabbcde",2)]
+	[TestCase("aabBcde",2)]
+	[TestCase("Indivisibility",1)]
+	[TestCase("Indivisibilities",2)]
+	public void TestMethod(string str, int expected) {
+		Assert.That(Kata.DuplicateCount(str), Is.EqualTo(expected));
+	}
+
+	[Test]
+	[TestCase("",0)]
+	[TestCase("abcde",0)]
+	[TestCase("aabbcde",2)]
+	[TestCase("aabBcde",2)]
+	[TestCase("Indivisibility",1)]
+	[TestCase("Indivisibilities",2)]
+	public void OneLaneTestMethod(string str, int expected) {
+		Assert.That(Kata.DuplicateCountOneLane(str), Is.EqualTo(expected));
+	}
+}
