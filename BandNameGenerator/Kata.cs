ad10ad52f211b21156d03e6827f6821bd987de9d
Rino <sherikawaii3390@gmail.com>
2022-05-17 05:03:59 +0800

    feat: Complete BandNameGenerator

---
+++ BandNameGenerator/Kata.cs
@@ -0,0 +1,9 @@
+﻿namespace Kata.BandNameGenerator;
+
+public class Kata
+{
+	public static string BandNameGenerator(string str) {
+		if (str[0] == str[^1]) return str[0].ToString().ToUpper() + str[1..] + str[1..];
+		return "The " + str[0].ToString().ToUpper() + str[1..];
+	}
+}
---
+++ BandNameGenerator/Test.cs
@@ -0,0 +1,18 @@
+﻿namespace Kata.BandNameGenerator;
+
+[TestFixture]
+public class Test
+{
+	[SetUp]
+	public void SetUp() {
+	}
+
+	[Test]
+	[TestCase("knife","The Knife")]
+	[TestCase("tart","Tartart")]
+	[TestCase("sandles","Sandlesandles")]
+	[TestCase("bed","The Bed")]
+	public void TestMethod(string noun,string expect) {
+		Assert.That(Kata.BandNameGenerator(noun),Is.EqualTo(expect));
+	}
+}
