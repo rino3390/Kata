bd52fd0ff5571771315f0c5fb1bddbe0e21cf692
Rino <sherikawaii3390@gmail.com>
2022-05-17 03:38:19 +0800

    feat: add new solution for String Average

--- StringAverage/Kata.cs
+++ StringAverage/Kata.cs
@@ -2,12 +2,21 @@
 
 public class Kata
 {
-	public string StringAverage(string str) {
-		var nums = str.Split(" ");
+	public string StringAverageWithEnum(string str) {
+		var nums = str.Split();
 		if (nums.Length==0 || nums.Any(num => !Enum.IsDefined(typeof(NumStringEnum),num))) {
 			return "n/a";
 		}
 		var sum = nums.Average(num => (int) Enum.Parse<NumStringEnum>(num));
 		return ((NumStringEnum) sum).ToString();
+	}
+	public string StringAverageWithArray(string str) {
+		var numString = new[] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+		var nums = str.Split();
+		if (nums.Length==0 || !nums.All(numString.Contains)) {
+			return "n/a";
+		}
+		var sum = nums.Average(num => Array.IndexOf(numString,num));
+		return numString[(int)sum];
 	}
 }
--- StringAverage/Tests.cs
+++ StringAverage/Tests.cs
@@ -14,7 +14,18 @@ public class Tests
 	[TestCase("zero zero zero zero zero","zero")]
 	[TestCase("one one eight one","two")]
 	[TestCase("","n/a")]
-	public void Test(string input,string expect) {
-		Assert.That(new Kata().StringAverage(input), Is.EqualTo(expect));
+	public void TestEnum(string input,string expect) {
+		Assert.That(new Kata().StringAverageWithEnum(input), Is.EqualTo(expect));
+	}
+	[Test]
+	[TestCase("zero nine five two","four")]
+	[TestCase("four six two three","three")]
+	[TestCase("one two three four five","three")]
+	[TestCase("five four","four")]
+	[TestCase("zero zero zero zero zero","zero")]
+	[TestCase("one one eight one","two")]
+	[TestCase("","n/a")]
+	public void TestArray(string input,string expect) {
+		Assert.That(new Kata().StringAverageWithArray(input), Is.EqualTo(expect));
 	}
 }
