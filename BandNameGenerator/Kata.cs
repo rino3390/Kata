3aa8f9f12f4cfe0853b8776f88385717445dc2b5
Rino <sherikawaii3390@gmail.com>
2022-05-17 05:06:00 +0800

    feat: change BandNameGenerator to one lane method

--- BandNameGenerator/Kata.cs
+++ BandNameGenerator/Kata.cs
@@ -3,7 +3,6 @@
 public class Kata
 {
 	public static string BandNameGenerator(string str) {
-		if (str[0] == str[^1]) return str[0].ToString().ToUpper() + str[1..] + str[1..];
-		return "The " + str[0].ToString().ToUpper() + str[1..];
+		return (str[0] != str[^1] ? "The " + char.ToUpper(str[0]) : char.ToUpper(str[0]) + str[1..]) + str[1..];
 	}
 }
