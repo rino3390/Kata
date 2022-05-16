ca9f3a41dca274ca51485baf4121f336226a3456
Rino <sherikawaii3390@gmail.com>
2022-05-17 04:40:57 +0800

    feat: Complete DeadAntCount

---
+++ DeadAnt/Kata.cs
@@ -0,0 +1,12 @@
+﻿namespace Kata.DeadAnt;
+
+public class Kata
+{
+	public static int DeadAntCount(string ants) {
+		if (string.IsNullOrEmpty(ants)) {
+			return 0;
+		}
+		ants = ants.Replace("ant", "");
+		return Math.Max(ants.Count(x=> x=='a'), Math.Max(ants.Count(x=> x=='n'),ants.Count(x=> x=='t')));
+	}
+}
---
+++ DeadAnt/Test.cs
@@ -0,0 +1,19 @@
+﻿namespace Kata.DeadAnt;
+
+[TestFixture]
+public class Test
+{
+	[SetUp]
+	public void SetUp() {
+	}
+
+	[Test]
+	[TestCase("ant ant ant ant",0)]
+	[TestCase(null,0)]
+	[TestCase("ant anantt aantnt",2)]
+	[TestCase("ant ant .... a nt",1)]
+	[TestCase("atta..aanat.at.nnt.antnt.tn.a.aan..a..n....t.n.nt.n.t",10)]
+	public void TestMethod(string ants,int expect) {
+		Assert.That(Kata.DeadAntCount(ants),Is.EqualTo(expect));
+	}
+}
